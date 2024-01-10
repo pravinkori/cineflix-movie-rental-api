980e61998f33cd2490658190104f524b5f0e8c64
Pravin Kori <pravinkori@protonmail.ch>
2024-01-10 16:52:12 +0530

    feat: Add PUT endpoints to update movies.

--- routes/movies.js
+++ routes/movies.js
@@ -48,3 +48,37 @@ router.post("/", async (req, res) => {
         }
     }
 });
+
+router.put("/:id", async (req, res) => {
+    const { error } = validateMovie(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const genre = await Genre.findById(req.body.genreID);
+    if (!genre) {
+        return res.status(404).send("Invalid genre ID");
+    }
+
+    const updateMovieByID = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+            title: req.body.title,
+            genre: {
+                _id: genre._id,
+                name: genre.name,
+            },
+            numberInStock: req.body.numberInStock,
+            dailyRentalRate: req.body.dailyRentalRate,
+        },
+        {
+            new: true,
+        }
+    );
+
+    if (!updateMovieByID) {
+        return res.status(404).send("The genre with given ID was not found");
+    }
+
+    res.send(updateMovieByID);
+});
