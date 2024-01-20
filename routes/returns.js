066d2131f8632fa4cc75df5adcb36a0254c71f59
Pravin Kori <pravinkori@protonmail.ch>
2024-01-20 15:30:36 +0530

    test: Add integration test to verify movieId and customerId.

--- routes/returns.js
+++ routes/returns.js
@@ -6,6 +6,13 @@ const router = express.Router();
 router.post(
     "/",
     asyncMiddleware(async (req, res, next) => {
+        if (!req.body.customerId) {
+            return res.status(400).send("customerId not provided");
+        }
+
+        if (!req.body.movieId) {
+            return res.status(400).send("movieId not provided");
+        }
         res.status(401).send("Unauthorized access");
     })
 );
