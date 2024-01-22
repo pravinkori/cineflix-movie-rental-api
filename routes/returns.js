a69947e836a613c8dd5a8513516fff5e20b1458d
Pravin Kori <pravinkori@protonmail.ch>
2024-01-22 20:17:55 +0530

    refactor: Add validation function logic as a middleware.

--- routes/returns.js
+++ routes/returns.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const moment = require("moment");
 const asyncMiddleware = require("../middleware/async.js");
 const auth = require("../middleware/auth.js");
+const validate = require("../middleware/validate.js");
 const { Rental } = require("../models/rentals.model.js");
 const { Movie } = require("../models/movies.model.js");
 
@@ -10,13 +11,8 @@ const router = express.Router();
 
 router.post(
     "/",
-    auth,
+    [auth, validate(validateReturn)],
     asyncMiddleware(async (req, res, next) => {
-        const { error } = validateReturn(req.body);
-        if (error) {
-            return res.status(400).send(error.details[0].message);
-        }
-
         const rental = await Rental.findOne({
             "customer._id": req.body.customerId,
             "movie._id": req.body.movieId,
